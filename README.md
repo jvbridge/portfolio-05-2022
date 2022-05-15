8ee375b56d89c5e1f60bf84206ea8f68e646eb39
Jonathan Bridge <jvbridge@gmail.com>
2022-05-14 21:26:12 -0700

    initializing all the files with starter code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # portfolio-05-2022
 The code for my deployed Heroku app with my portfolio on it
+
+Full deployed site will be at jvbridge-herokuapp.com
+
+## License
+
+Copyright Jonathan Bridge 2022
+
+MIT license
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+
+const routes = require("./controllers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`\nServer running on port ${PORT}`);
+});
